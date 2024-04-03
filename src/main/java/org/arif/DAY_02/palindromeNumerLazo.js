b829529bd74f796cd917ea037bd20e1f47aff377
Lazkin-maker <layo1900@student.miun.se>
2024-04-03 12:06:35 +0200

    updated kata

--- src/main/java/org/arif/DAY_02/palindromeNumerLazo.js
+++ src/main/java/org/arif/DAY_02/palindromeNumerLazo.js
@@ -1,24 +1,23 @@
-// function isPalindrome(x) {
-//     let str_x = x.toString();
-//     return str_x === str_x.split('').reverse().join('');
-// }
+function isPalindrome(x) {
+    let str_x = x.toString();
+    return str_x === str_x.split('').reverse().join('');
+}
+
+let x = 121;
+console.log(isPalindrome(x)); 
 
 
-// let x = 121;
-// console.log(isPalindrome(x)); 
-
-
-var isPalindrome = function(x) {
-    let originArr = x.toString().split('')
+// var isPalindrome = function(x) {
+//     let originArr = x.toString().split('')
   
-    let reversedArr = [...originArr].reverse();
+//     let reversedArr = [...originArr].reverse();
     
-    if(originArr.join('') === reversedArr.join('')){
-        return true
-    }else{
-        return false
-    }
-};
+//     if(originArr.join('') === reversedArr.join('')){
+//         return true
+//     }else{
+//         return false
+//     }
+// };
 
 //isPalindrome(12134)
 console.log(isPalindrome(121))
