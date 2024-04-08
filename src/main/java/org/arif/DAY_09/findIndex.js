e39ae35f90ff76a4128302489337650bd5be4475
Arif Hossain <hossaininfo@ymail.com>
2024-04-08 09:03:09 +0200

    find the index of the first

---
+++ src/main/java/org/arif/DAY_09/findIndex.js
@@ -0,0 +1,15 @@
+
+/**
+ * @link: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+const strStr = function(haystack, needle) {
+
+};
+
+
+const result = strStr("sadbutsad", "sad");
+const result1 = strStr("leetcode", "leeto");
+console.log(result);
