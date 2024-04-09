d547b1ab5f658c0bd3b6a13fe8191b101a2075a1
Lazkin-maker <layo1900@student.miun.se>
2024-04-09 10:05:23 +0200

    find index kata Lazo

---
+++ src/main/java/org/arif/DAY_09/findIndexLazo.js
@@ -0,0 +1,31 @@
+var strStr = function(haystack, needle) {
+    let haystackArr = haystack.split('');
+    let needleArr = needle.split('');
+    
+    let firstIndex = needleArr[0];
+    
+    let currentChar =haystackArr.find((char) => char === firstIndex)
+
+    if(currentChar){
+        let i = 1;
+        let j = haystackArr.indexOf(currentChar) +1
+        console.log(i + ",,,," + j)
+        while(i < needleArr.length && j < haystackArr.length){
+            if(needleArr[i] === haystackArr[j]){
+                i++;
+                j++
+            }else{
+                return -1;
+               
+            }
+        }
+        return haystackArr.indexOf(currentChar)
+    }
+
+};
+
+
+let haystackStr = "sadbutsad";
+let needleStr = "sadg";
+
+console.log(strStr(haystackStr, needleStr))
